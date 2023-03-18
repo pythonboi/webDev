b2917884b884737a73d2acdb59d1c4f7274aa33d
pythonboi <hafeeztukur@gmail.com>
2023-03-18 01:58:36 -0400

    added footer to the existing code

--- 3Imams/imams/imams.py
+++ 3Imams/imams/imams.py
@@ -5,15 +5,18 @@ imams = Flask(__name__)
 
 @imams.route("/")
 def hello_world():
-    # return render_template("imam.html")
-    return render_template("main.html")
+    # return render_template("historical.html")
+    return render_template("base.html")
 
 
 @imams.route("/imams")
-def his_imam():
-    return render_template("imam.html")
+def the_imams():
+    return render_template("Imam.html")
 
 
+@imams.route("/historical")
+def historical():
+    return render_template("historical.html")
 
 # if __name__ == '__main__':
 #     imams.run(host='0.0.0.0', debug=True)
